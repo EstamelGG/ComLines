770429ff43fdacac5a525d9f851f4d8c1fe50a2a
Sakari <20642596+sakarie9@users.noreply.github.com>
2024-05-12 10:32:18 +0800

    refactor: extract routertabs

---
+++ src/components/RouterTabs.tsx
@@ -0,0 +1,36 @@
+import { createFromIconfontCN } from '@ant-design/icons';
+import React from 'react';
+import About from './About';
+import ReverseShell from './revShell/ReverseShell';
+
+const IconFont = createFromIconfontCN({
+  scriptUrl: ['./iconfont.js']
+});
+
+interface IRouterComponent {
+  key: string;
+  icon: JSX.Element;
+  name: string;
+  path: string;
+  component: React.ComponentType<any>;
+}
+
+const Tabs: Array<IRouterComponent> = [
+  {
+    key: '0',
+    icon: <IconFont type='icon-about' style={{ fontSize: '1.5em', marginTop: 3 }} />,
+    name: 'About',
+    path: '/About',
+    component: About
+  },
+  {
+    key: '1',
+    icon: <IconFont type='icon-gnubash' style={{ fontSize: '1.5em', marginTop: 3 }} />,
+    name: 'Reverse Shell',
+    path: '/RevShell',
+    component: ReverseShell
+  }
+];
+
+
+export { Tabs };
